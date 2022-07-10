9af8a8cdc38f24943853b9af62384bcd113b4bbf
andrew <andrewhughes1988@gmail.com>
2022-07-10 17:54:16 -0500

    Added optional command line arguments and command line help page using System.CommandLine

--- PassGen/Program.cs
+++ PassGen/Program.cs
@@ -1,3 +1,24 @@
-﻿using PassGen;
+﻿using System.CommandLine;
+using PassGen;
 
-Console.WriteLine(Generator.Generate(length: 20));
+
+byte defaultPasswordLength = 20;
+
+//Setting up the command line arguments, options, and help system
+var rootCommand = new RootCommand("A simple Command-Line password generator");
+var lengthOption = new Option<byte>(
+    name: "--length",
+    description: "Enter the password length you want to generate.\n" +
+                 "Must be a positive integer from 0 - 255",
+    getDefaultValue: () => defaultPasswordLength);
+
+lengthOption.AddAlias("-l");
+rootCommand.Add(lengthOption);
+
+rootCommand.SetHandler((byte lengthOptionValue) =>
+{
+    Console.WriteLine(Generator.Generate(length: lengthOptionValue));
+}, 
+lengthOption);
+
+rootCommand.Invoke(args);
